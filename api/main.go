b64d3d5de2ae8a0010eeb1cefdde4bfbe55e0316
YhyaGithub <yhyamobile57@gmail.com>
2023-06-16 09:38:06 +0300

    feat(routes): initialize shorten route

--- api/main.go
+++ api/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/yhyadev/links-shortener/routes"
+)
 
 func main() {
   app := fiber.New()
+
+  routes.Setup(app)
 
   app.Listen(":8080")
 }
